6323ede6f78bfdd4261bd995a1d4476363417faf
judy-kim <57675076+judy-kim@users.noreply.github.com>
2022-11-04 14:27:31 +0000

    Deploying to gh-pages from @ AdobeDocs/target-developers@de9d67f16e6d2397ad2bdda570003e60af71acda 🚀

---
+++ component---src-pages-administer-admin-api-index-md-36c3491363d2503a0928.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktarget_developers=self.webpackChunktarget_developers||[]).push([[822],{41900:function(t,e,n){n.r(e),n.d(e,{_frontmatter:function(){return c},default:function(){return m}});var r=n(87462),o=n(63366),u=(n(15007),n(64983)),s=n(91515),a=["components"],c={},p={_frontmatter:c},f=s.Z;function m(t){var e=t.components,n=(0,o.Z)(t,a);return(0,u.mdx)(f,(0,r.Z)({},p,n,{components:e,mdxType:"MDXLayout"}))}m.isMDXComponent=!0}}]);
+//# sourceMappingURL=component---src-pages-administer-admin-api-index-md-36c3491363d2503a0928.js.map
